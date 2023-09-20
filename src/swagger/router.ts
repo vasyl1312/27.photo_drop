2484f411884008145f29f384ece736ff79d94804
vasyl1312 <vasylhryts@knu.ua>
2023-09-20 15:46:04 +0300

    try to fix deploy

--- src/swagger/router.ts
+++ src/swagger/router.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import * as swaggerUi from 'swagger-ui-express'
 import swaggerDocs from './swagger.json'
-
 const router = express.Router()
 
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css' //styles for swagger
+
 router.use('/', swaggerUi.serve)
-router.get('/', swaggerUi.setup(swaggerDocs))
+router.get('/', swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL }))
 
 export const swaggerRouter = router
