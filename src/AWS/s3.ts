b3c4318c66c9635e627cd566504b40db4e36a65c
vasyl1312 <vasylhryts@knu.ua>
2023-09-27 18:51:15 +0300

    upload photo to s3

---
+++ src/AWS/s3.ts
@@ -0,0 +1,45 @@
+import * as dotenv from 'dotenv'
+import * as fs from 'fs'
+import { S3 } from 'aws-sdk'
+
+dotenv.config()
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET_KEY
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+})
+
+// Uploads a file to S3
+function uploadFile(file: Express.Multer.File) {
+  const fileStream = fs.createReadStream(file.path)
+
+  const uploadParams: S3.Types.PutObjectRequest = {
+    Bucket: bucketName!,
+    Body: fileStream,
+    Key: file.filename,
+  }
+
+  return s3.upload(uploadParams).promise()
+}
+
+// Downloads a file from S3
+function getFileStream(fileKey: string) {
+  if (!bucketName) {
+    throw new Error('Bucket name is not defined.')
+  }
+
+  const downloadParams: S3.Types.GetObjectRequest = {
+    Key: fileKey,
+    Bucket: bucketName,
+  }
+
+  return s3.getObject(downloadParams).createReadStream()
+}
+
+export { uploadFile, getFileStream }
--- src/routes/albumsRouter.ts
+++ src/routes/albumsRouter.ts
@@ -4,6 +4,8 @@ import express, { Request, Response } from 'express'
 import { sql } from 'drizzle-orm'
 import { albums, TNewAlbums } from '../db/schema/albums'
 import { photographersSchema } from '../db/schema/photographer'
+import { uploadFile, getFileStream } from '../AWS/s3'
+import multer from 'multer'
 
 const router = express.Router()
 
@@ -12,6 +14,8 @@ interface DecodedToken {
   iat: number
   exp: number
 }
+
+const upload = multer({ dest: 'uploads/' })
 
 const albumsRouter = () => {
   router.post('/', async (req: Request, res: Response) => {
@@ -53,48 +57,79 @@ const albumsRouter = () => {
       console.error('Error:', error)
       return res.status(500).send('Internal Server Error')
     }
-  }),
-    router.get('/', async (req: Request, res: Response) => {
-      try {
-        const token = req.header('Authorization')?.replace('Bearer ', '')
-        if (!token) {
-          return res.status(401).json({ error: 'Token is required' })
-        }
-        const decoded = jwt.verify(token, `${process.env.SECRET_KEY}`) as DecodedToken
-        const userId = decoded.userId
+  })
 
-        const db = await connect()
-        const user = await db
-          .select()
-          .from(photographersSchema)
-          .where(sql`${photographersSchema.id} = ${userId}`)
-          .execute()
-
-        if (!user || user.length === 0) {
-          return res.status(403).json({ error: 'Not authorized' })
-        }
-
-        const allAlbums = await db.select().from(albums).execute()
-
-        const albumsPerPage = 20
-        const maxPage = Math.ceil(allAlbums.length / albumsPerPage)
-
-        const page = parseInt(req.query.page as string) || 1
-        const startIdx = (page - 1) * albumsPerPage
-        const endIdx = startIdx + albumsPerPage
-        const pagedAlbums = allAlbums.slice(startIdx, endIdx)
-
-        const response = {
-          maxPage,
-          albums: pagedAlbums,
-        }
-
-        return res.status(200).json(response)
-      } catch (error) {
-        console.error('Error:', error)
-        return res.status(500).send('Internal Server Error')
+  router.get('/', async (req: Request, res: Response) => {
+    try {
+      const token = req.header('Authorization')?.replace('Bearer ', '')
+      if (!token) {
+        return res.status(401).json({ error: 'Token is required' })
       }
-    })
+      const decoded = jwt.verify(token, `${process.env.SECRET_KEY}`) as DecodedToken
+      const userId = decoded.userId
+
+      const db = await connect()
+      const user = await db
+        .select()
+        .from(photographersSchema)
+        .where(sql`${photographersSchema.id} = ${userId}`)
+        .execute()
+
+      if (!user || user.length === 0) {
+        return res.status(403).json({ error: 'Not authorized' })
+      }
+
+      const allAlbums = await db.select().from(albums).execute()
+
+      const albumsPerPage = 20
+      const maxPage = Math.ceil(allAlbums.length / albumsPerPage)
+
+      const page = parseInt(req.query.page as string) || 1
+      const startIdx = (page - 1) * albumsPerPage
+      const endIdx = startIdx + albumsPerPage
+      const pagedAlbums = allAlbums.slice(startIdx, endIdx)
+
+      const response = {
+        maxPage,
+        albums: pagedAlbums,
+      }
+
+      return res.status(200).json(response)
+    } catch (error) {
+      console.error('Error:', error)
+      return res.status(500).send('Internal Server Error')
+    }
+  })
+
+  router.post('/:id/photos', upload.single('image'), async (req: Request, res: Response) => {
+    try {
+      const albumId = req.params.id
+      const file = req.file
+      console.log(file)
+
+      if (!file) {
+        return res.status(400).json({ error: 'No file uploaded' })
+      }
+
+      const result = await uploadFile(file)
+      console.log(result)
+
+      return res.status(200).json({ albumId })
+    } catch (error) {
+      console.error('Error:', error)
+      return res.status(500).send('Internal Server Error')
+    }
+  })
+
+  router.get('/:id/photos/:key', (req, res) => {
+    //http://localhost:5000/albums/7/photos/01a3597bc0eedbd0ac15da64a928a0d2
+    console.log(req.params)
+    const albumId = req.params.id
+    const key = req.params.key
+    const readStream = getFileStream(key)
+
+    readStream.pipe(res)
+  })
 
   return router
 }
