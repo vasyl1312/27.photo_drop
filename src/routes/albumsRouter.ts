24d69aba26e1ef8173fdf1271c6ad82ec9ce5834
vasyl1312 <vasylhryts@knu.ua>
2023-09-27 12:10:15 +0300

    add get route for albums

--- src/routes/albumsRouter.ts
+++ src/routes/albumsRouter.ts
@@ -53,7 +53,48 @@ const albumsRouter = () => {
       console.error('Error:', error)
       return res.status(500).send('Internal Server Error')
     }
-  })
+  }),
+    router.get('/', async (req: Request, res: Response) => {
+      try {
+        const token = req.header('Authorization')?.replace('Bearer ', '')
+        if (!token) {
+          return res.status(401).json({ error: 'Token is required' })
+        }
+        const decoded = jwt.verify(token, `${process.env.SECRET_KEY}`) as DecodedToken
+        const userId = decoded.userId
+
+        const db = await connect()
+        const user = await db
+          .select()
+          .from(photographersSchema)
+          .where(sql`${photographersSchema.id} = ${userId}`)
+          .execute()
+
+        if (!user || user.length === 0) {
+          return res.status(403).json({ error: 'Not authorized' })
+        }
+
+        const allAlbums = await db.select().from(albums).execute()
+
+        const albumsPerPage = 20
+        const maxPage = Math.ceil(allAlbums.length / albumsPerPage)
+
+        const page = parseInt(req.query.page as string) || 1
+        const startIdx = (page - 1) * albumsPerPage
+        const endIdx = startIdx + albumsPerPage
+        const pagedAlbums = allAlbums.slice(startIdx, endIdx)
+
+        const response = {
+          maxPage,
+          albums: pagedAlbums,
+        }
+
+        return res.status(200).json(response)
+      } catch (error) {
+        console.error('Error:', error)
+        return res.status(500).send('Internal Server Error')
+      }
+    })
 
   return router
 }
